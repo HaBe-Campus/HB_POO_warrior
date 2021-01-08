6a5c95676d28097ae4942533a1417d3e4ef48a43
Hamid BERRAHAL <hamid.berrahal@Digital-Grenoble.local>
2021-01-08 10:40:38 +0100

    updated file

--- students/battleField.php
+++ students/battleField.php
@@ -6,6 +6,24 @@
 
   class BattleField extends BaseBattleField
   {
+    public static function createMyWarrior(){
+        $warrior= new StartrekWarrior($GLOBALS['warriorID']);
+        $weapon = new Weapon('Coup de Frein à Main',100);
+        $weapon->setImageUrl('https://www.google.com/url?sa=i&url=https%3A%2F%2Finitiald.fandom.com%2Fwiki%2FBunta_Fujiwara&psig=AOvVaw26kSmWwU7uNKmyknAvtzQ8&ust=1610118010946000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNDzkLOKiu4CFQAAAAAdAAAAABAE');
+        $warrior->setWeapon($weapon);
+        $warrior->setImageUrl('https://www.google.com/url?sa=i&url=https%3A%2F%2Finitiald.fandom.com%2Fwiki%2FBunta_Fujiwara&psig=AOvVaw26kSmWwU7uNKmyknAvtzQ8&ust=1610118010946000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNDzkLOKiu4CFQAAAAAdAAAAABAE');
+        $warrior->saveNew();
+        
 
+    }
+    public static function createOtherWarrior(){
+      $warrior= new StartrekWarrior('Hamid');
+      $weapon = new Weapon('Coup de Frein à Main',100);
+      $weapon->setImageUrl('https://www.google.com/url?sa=i&url=https%3A%2F%2Finitiald.fandom.com%2Fwiki%2FBunta_Fujiwara&psig=AOvVaw26kSmWwU7uNKmyknAvtzQ8&ust=1610118010946000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNDzkLOKiu4CFQAAAAAdAAAAABAE');
+      $warrior->setWeapon($weapon);
+      $warrior->setImageUrl('https://www.google.com/url?sa=i&url=https%3A%2F%2Finitiald.fandom.com%2Fwiki%2FBunta_Fujiwara&psig=AOvVaw26kSmWwU7uNKmyknAvtzQ8&ust=1610118010946000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNDzkLOKiu4CFQAAAAAdAAAAABAE');
+      $warrior->saveNew();
+      
 
   }
+}
--- students/warrior.php
+++ students/warrior.php
@@ -2,10 +2,84 @@
 
 require_once __DIR__ . "/../base/localWarrior.php";
 
+$GLOBALS['warriorID'] = 'yolesbg';
 
-$GLOBALS['warriorID'] = 'azertyuiop';
 
+//class Warrior extends StartrekWarrior{};
 // Définissez vos class de combattants
-abstract class Warrior extends LocalWarrior {
 
+
+abstract class Warrior extends LocalWarrior {
+  
+   public $id;
+   public $name;
+   public $speed=30;
+   public $life=100;
+   public $shield=20;
+   public $imageUrl;
+   public $weapon;
+
+   public function __construct($id){
+        $this->id = $id;
+   }
+  public function setWeapon(Weapon $weapon){
+      $this->weapon=$weapon;
+
+  }
+  public function setImageUrl($url){
+      $this->imageUrl = $url;
+  }
 };
+
+class StartrekWarrior extends Warrior{
+    public $mentalPower = 8;
+    public function __construct($id){
+        parent::__construct($id);
+    }
+    
+    
+    public function power()
+    {
+        return $this->mentalPower;
+    }
+}
+
+class MarvelWarrior extends Warrior
+{
+    public $superPower = 100;
+    public function __construct($id){
+        parent::__construct($id);
+    }
+    
+    public function power()
+    {
+         return $this->superPower;
+    }
+}
+class PokemonWarrior extends Warrior
+{
+    public $level = 1;
+    public function __construct($id){
+        parent::__construct($id);
+    }
+   
+    public function power()
+    {
+        return $this->level;
+    }
+}
+
+class Weapon{
+    public $id;
+    public $strength;
+    public $imageUrl;
+
+    public function __construct($id,$strength){
+        $this->id=$id;
+        $this->strength=$strength;
+    }
+    public function setImageUrl($url){
+        $this->imageUrl = $url;
+    }
+}
+
